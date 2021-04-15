b74bcab8bbb5062943f4936b0677641dd49f7c7d
qudrnr217 <dksk1233@naver.com>
2021-04-15 15:38:11 +0900

    change print message in foo()

--- src/foo.c
+++ src/foo.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "funcs.h"
 
-void foo()
+int foo()
 {
-	printf("hello foo\n");
+	int flag=0;
+	if(flag){
+		
+		printf("hello foo\n");
+	}else{
+		printf("good bye~\n");
+	}
+
+	return 0;
 }
